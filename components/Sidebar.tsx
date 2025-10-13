a68fda2c31011422c78d588c07cb1f0c4a642575
dev-caw <davison.menezes@caw.adv.br>
2025-10-13 03:41:54 -0300

    Primeiro commit: início do projeto

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -52,7 +52,11 @@ const Sidebar: React.FC<SidebarProps> = ({ modules, activeModule, isExpanded, on
 
       <div className="p-4 text-center">
          <div className={`text-xs text-slate-500 mt-4 transition-opacity duration-300 ${isExpanded ? 'opacity-100' : 'opacity-0'}`}>
-            &copy; 2025 INPI
+                        <img 
+                src="https://dmenezes007.github.io/pgi-inpi/files/imgs/logo_inpi_branco_fundo_transparente.png" 
+                alt="Logo do INPI" 
+                className="h-8"
+            />
          </div>
       </div>
     </aside>
