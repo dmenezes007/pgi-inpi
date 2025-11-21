87206135a5c09d38ae8983b72b6ca7a256e986f3
dev-caw <davison.menezes@caw.adv.br>
2025-11-21 17:48:47 -0300

    Primeiro commit: início do projeto

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -63,7 +63,7 @@ const MainContent: React.FC<MainContentProps> = ({ activeModule, onModuleSelect,
 
   return (
     <main className="flex-1 p-4 sm:p-8 overflow-y-auto bg-slate-900">
-      <div className="max-w-7xl mx-auto">
+      <div>
         {renderModule()}
       </div>
     </main>
